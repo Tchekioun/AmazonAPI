52810339538a38dc79a1fd978855a775ed842dce
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-04 17:14:04 +0100

    add local auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './services/auth.service';
 
 @Module({
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, { provide: APP_GUARD, useClass: LocalAuthGuard }],
 })
 export class AuthModule {}
