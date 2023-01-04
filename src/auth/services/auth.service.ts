20c4975a1c118ab3de1731a7e338af42c1a40e89
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-04 16:25:36 +0100

    add validate method to auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { PasswordService } from './password.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private passwordService: PasswordService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findByEmail(username);
+    const isMatched = await this.passwordService.compare(
+      password,
+      user.password,
+    );
+    if (isMatched) {
+      const { password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+}
