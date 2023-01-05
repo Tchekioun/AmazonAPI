002c4cba25a9380cd6798e4d68865de1ce691786
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-05 10:22:01 +0100

    connect login to the auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
-import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './services/auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { env } from 'process';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, { provide: APP_GUARD, useClass: LocalAuthGuard }],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: env.jwtPassword,
+      signOptions: { expiresIn: '120s' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
