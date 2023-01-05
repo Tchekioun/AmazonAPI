83bade8a9a020db8c5cae073c47863a971bd790f
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-05 12:47:57 +0100

    jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { env } from 'process';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: env.PASSWORD,
+    });
+  }
+
+  async validate(payload: Omit<User, 'password'>) {
+    return { userId: payload.id, name: payload.name };
+  }
+}
