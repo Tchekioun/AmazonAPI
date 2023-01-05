828b9b4d35f96e51c7c719c2d75d612b9ccad5d3
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-05 12:52:39 +0100

    jwt guard and add it to auth module and remove some unnecessary some code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,21 @@ import { AuthService } from './services/auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt/dist';
 import { env } from 'process';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PasswordService } from './services/password.service';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: env.jwtPassword,
+      secret: env.PASSWORD,
       signOptions: { expiresIn: '120s' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
+  exports: [PasswordService],
 })
 export class AuthModule {}
