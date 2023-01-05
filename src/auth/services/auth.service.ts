03144fc3f783ac336dc06abf9b34ad50c377af36
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-05 10:16:17 +0100

    add jwt login service and assign token

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
 import { PasswordService } from './password.service';
 
@@ -7,6 +9,7 @@ export class AuthService {
   constructor(
     private usersService: UsersService,
     private passwordService: PasswordService,
+    private jwtService: JwtService,
   ) {}
 
   async validateUser(username: string, password: string) {
@@ -20,5 +23,10 @@ export class AuthService {
       return rest;
     }
     return null;
+  }
+
+  async login(user: Omit<User, 'password'>) {
+    const payload = { name: user.name, sub: user.id };
+    return { access_token: this.jwtService.sign(payload) };
   }
 }
